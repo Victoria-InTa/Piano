824c12550381ed5e188accec7c09a4cf52297935
Victoria_T <Tabordaingridvictoria@outlook.es>
2021-12-15 21:14:28 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,51 @@
+let btn_1 = document.querySelector('.teclas1')
+let btn_2 = document.querySelector('.teclas2')
+let btn_3 = document.querySelector('.teclas3')
+let btn_4 = document.querySelector('.teclas4')
+let btn_5 = document.querySelector('.teclas5')
+let btn_6 = document.querySelector('.teclas6')
+let btn_7 = document.querySelector('.teclas7')
+let btn_8 = document.querySelector('.teclas8')
+
+
+
+
+btn_1.addEventListener("click", () => {
+    var audio = new Audio('sonido.mp3');
+    audio.play('sonido.mp3');
+})
+
+btn_2.addEventListener("click", () => {
+    var audio = new Audio('sonido2.mp3');
+    audio.play('sonido2.mp3');
+})
+
+btn_3.addEventListener("click", () => {
+    var audio = new Audio('sonido3.mp3');
+    audio.play('sonido3.mp3');
+})
+
+btn_4.addEventListener("click", () => {
+    var audio = new Audio('sonido4.mp3');
+    audio.play('sonido4.mp3');
+})
+
+btn_5.addEventListener("click", () => {
+    var audio = new Audio('sonido5.mp3');
+    audio.play('sonido5.mp3');
+})
+
+btn_6.addEventListener("click", () => {
+    var audio = new Audio('sonido6.mp3');
+    audio.play('sonido6.mp3');
+})
+
+btn_7.addEventListener("click", () => {
+    var audio = new Audio('sonido7.mp3');
+    audio.play('sonido7.mp3');
+})
+
+btn_8.addEventListener("click", () => {
+    var audio = new Audio('sonido8.mp3');
+    audio.play('sonido8.mp3');
+})
